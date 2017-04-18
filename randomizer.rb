a8f0e87c6ea7e43482b6a2d09b30798ce75538de
LagoLunatic <LagoLunatic@users.noreply.github.com>
2017-04-18 19:04:58 -0400

    Fixes to player randomizer

--- randomizer.rb
+++ randomizer.rb
@@ -778,12 +778,26 @@ class Randomizer
     players = game.players
     
     players.each do |player|
-      player["Walking speed"]     =  rng.rand(0x1400..0x3000)
-      player["Jump force"]        = -rng.rand(0x5A00..0x6000)
-      player["Double jump force"] = -rng.rand(0x4A00..0x6000)
-      player["Slide force"]       =  rng.rand(0x1800..0x5000)
-      player["Backdash force"]    = -rng.rand(0x1000..0x5000)
-      player["Backdash duration"] =  rng.rand(20..60)
+      player["Walking speed"]       =  rng.rand(0x1400..0x3000)
+      player["Jump force"]          = -rng.rand(0x5A00..0x6000)
+      player["Double jump force"]   = -rng.rand(0x4A00..0x6000)
+      player["Slide force"]         =  rng.rand(0x1800..0x5000)
+      player["Backdash force"]      = -rng.rand(0x3800..0x5800)
+      player["Backdash friction"]   =  rng.rand(0x100..0x230)
+      player["Backdash duration"]   =  rng.rand(20..60)
+      player["Trail scale"]         =  rng.rand(0x0D00..0x1200)
+      player["Enable player scale"] =  rng.rand(0..1)
+      player["Player height scale"] =  player["Trail scale"]
+      player["Number of trails"]    =  rng.rand(0x00..0x14)
+      
+      ["Trail start color", "Trail end color"].each do |attr_name|
+        color = 0
+        color |= rng.rand(0..0x1F)
+        color |= rng.rand(0..0x1F) << 8
+        color |= rng.rand(0..0x1F) << 16
+        color |= rng.rand(0..0x1F) << 24
+        player[attr_name] = color
+      end
       
       [
         "Actions",
@@ -801,34 +815,37 @@ class Randomizer
     end
     
     # Shuffle some player attributes such as graphics
-    remaining_players = players.dup
-    players.each do |player|
-      next unless remaining_players.include?(player) # Already randomized this player
-      
-      remaining_players.delete(player)
-      
-      break if remaining_players.empty?
-      
-      other_player = remaining_players.sample(random: rng)
-      remaining_players.delete(other_player)
-      
-      [
-        "GFX list pointer",
-        "Sprite pointer",
-        "Palette pointer",
-        "State anims ptr",
-        "GFX file index",
-        "Sprite file index",
-        "Sprite Y offset",
-        "Hitbox pointer",
-        "Face icon frame",
-      ].each do |attr_name|
-        player[attr_name], other_player[attr_name] = other_player[attr_name], player[attr_name]
-      end
-      
-      # Horizontal flip bit
-      player["??? bitfield"][0], other_player["??? bitfield"][0] = other_player["??? bitfield"][0], player["??? bitfield"][0]
-    end
+    #remaining_players = players.dup
+    #players.each do |player|
+    #  next unless remaining_players.include?(player) # Already randomized this player
+    #  
+    #  remaining_players.delete(player)
+    #  
+    #  break if remaining_players.empty?
+    #  
+    #  other_player = remaining_players.sample(random: rng)
+    #  remaining_players.delete(other_player)
+    #  
+    #  [
+    #    "GFX list pointer",
+    #    "Sprite pointer",
+    #    "Palette pointer",
+    #    "State anims ptr",
+    #    "GFX file index",
+    #    "Sprite file index",
+    #    "Filename pointer",
+    #    "Sprite Y offset",
+    #    "Hitbox pointer",
+    #    "Face icon frame",
+    #    "Unknown 21",
+    #    "Unknown 22",
+    #  ].each do |attr_name|
+    #    player[attr_name], other_player[attr_name] = other_player[attr_name], player[attr_name]
+    #  end
+    #  
+    #  # Horizontal flip bit
+    #  player["??? bitfield"][0], other_player["??? bitfield"][0] = other_player["??? bitfield"][0], player["??? bitfield"][0]
+    #end
     
     players.each do |player|
       player["Actions"][1] = true # Can use weapons
