bdca66d9e9085441226b75c228826cee3abc6ec4
LagoLunatic <LagoLunatic@users.noreply.github.com>
2017-05-26 16:35:35 -0400

    Weapon synth rando: Only allow upgrading into item of same slot
    This prevents a bug where the game would auto-equip the item into the
    wrong slot if you upgraded something you're wearing into a different
    type of item.

--- randomizers/extra_randomizers.rb
+++ randomizers/extra_randomizers.rb
@@ -355,12 +355,32 @@ module ExtraRandomizers
   def randomize_weapon_synths
     return unless GAME == "dos"
     
+    items_by_type = []
+    items_by_type << (0x00..0x41).to_a # Consumables
+    items_by_type << (0x42..0x90).to_a # Weapons
+    items_by_type << (0x91..0xAE).to_a # Body armor
+    items_by_type << (0xAF..0xCD).to_a # Accessories
+    items_by_type.map! do |item_list_for_type|
+      item_list_for_type & all_non_progression_pickups
+    end
+    
     WEAPON_SYNTH_CHAIN_NAMES.each_index do |index|
       chain = WeaponSynthChain.new(index, game.fs)
+      
+      item_types_with_enough_items = items_by_type.select do |item_list_for_type|
+        item_list_for_type.length >= chain.synths.length*2
+      end
+      items_available_for_this_chain = item_types_with_enough_items.sample(random: rng)#.dup
+      
       chain.synths.each do |synth|
-        synth.required_item_id = rng.rand(ITEM_GLOBAL_ID_RANGE) + 1
+        req_item = items_available_for_this_chain.sample(random: rng)
+        items_available_for_this_chain.delete(req_item)
+        created_item = items_available_for_this_chain.sample(random: rng)
+        items_available_for_this_chain.delete(created_item)
+        
+        synth.required_item_id = req_item + 1
         synth.required_soul_id = rng.rand(SKILL_LOCAL_ID_RANGE)
-        synth.created_item_id = rng.rand(ITEM_GLOBAL_ID_RANGE) + 1
+        synth.created_item_id = created_item + 1
         
         synth.write_to_rom()
       end
