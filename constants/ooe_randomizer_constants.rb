ba2d7cdc9db208351b26fac3bd29d5cc4cbc87be
LagoLunatic <LagoLunatic@users.noreply.github.com>
2017-06-04 19:48:41 -0400

    Boss randomizer: Don't randomize gravedorcus

--- constants/ooe_randomizer_constants.rb
+++ constants/ooe_randomizer_constants.rb
@@ -11,4 +11,5 @@ SPAWNER_ENEMY_IDS = [0x00, 0x01, 0x03, 0x0B, 0x0F, 0x1B, 0x2B, 0x3E, 0x48, 0x60,
 
 RANDOMIZABLE_BOSS_IDS = BOSS_IDS -
   [0x78] - # Remove the final boss, Dracula. 
-  [0x6D, 0x76] # Also remove Brachyura and Eligor since they need their own huge rooms.
+  [0x6D, 0x76] - # Also remove Brachyura and Eligor since they need their own huge rooms.
+  [0x71] # Remove Gravedorcus because he relies on code and objects specific to Oblivion Ridge.
