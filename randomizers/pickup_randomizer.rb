4bde043a1af6c11a18547542b3299b56884517f8
LagoLunatic <LagoLunatic@users.noreply.github.com>
2017-09-24 11:25:01 -0400

    Portrait rando: Remove cutscene that needs city of haze portrait
    fixes #28

--- randomizers/pickup_randomizer.rb
+++ randomizers/pickup_randomizer.rb
@@ -166,6 +166,12 @@ module PickupRandomizer
       forest_cutscene = game.entity_by_str("00-08-01_03")
       forest_cutscene.type = 0
       forest_cutscene.write_to_rom()
+      
+      # Remove the cutscene where the player characters talk about the first portrait they find that leads to City of Haze.
+      # It bugs out the game pretty seriously if there's no portrait in the room.
+      forest_cutscene = game.entity_by_str("00-01-00_01")
+      forest_cutscene.type = 0
+      forest_cutscene.write_to_rom()
     end
   end
   
