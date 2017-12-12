a773f5f75eea3587831c24209c33a38a48b0962c
LagoLunatic <LagoLunatic@users.noreply.github.com>
2017-12-12 04:12:52 -0500

    Fix DoS infinite magical tickets causing error

--- randomizer.rb
+++ randomizer.rb
@@ -1076,8 +1076,8 @@ class Randomizer
     room_index = @starting_room.room_index
     case GAME
     when "dos"
-      game.fs.write(0x023E0114, [sector_index].pack("C"))
-      game.fs.write(0x023E0118, [room_index].pack("C"))
+      game.fs.write(0x02308920+0x14, [sector_index].pack("C"))
+      game.fs.write(0x02308920+0x18, [room_index].pack("C"))
     when "por"
       game.fs.write(0x0203A280, [0xE3A00000].pack("V")) # Change mov to constant mov instead of register mov
       game.fs.write(0x0203A280, [area_index].pack("C"))
