2f7710f49e97ea20660f911936fbb6128127c765
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-02-03 23:34:12 -0500

    Enable some more options forcibly for map rando

--- randomizer.rb
+++ randomizer.rb
@@ -358,6 +358,13 @@ class Randomizer
       options[:unlock_boss_doors] = true
       options[:add_magical_tickets] = true
     end
+    if options[:randomize_rooms_map_friendly]
+      # The map rando won't necessarily place the rooms containing the normal bosses/portraits/villagers.
+      # So forcibly enable these options so these things can be placed wherever.
+      options[:randomize_boss_souls] = true
+      options[:randomize_portraits] = true
+      options[:randomize_villagers] = true
+    end
     
     apply_pre_randomization_tweaks()
     
