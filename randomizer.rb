7bd8e3bacf3c70425dd2fa58de6cd33ae0a4b05a
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-02-06 19:47:18 -0500

    Fix bug when forcibly enabling some options for map rando compatibility

--- randomizer.rb
+++ randomizer.rb
@@ -198,6 +198,18 @@ class Randomizer
     end
     
     if room_rando?
+      options[:unlock_boss_doors] = true
+      options[:add_magical_tickets] = true
+    end
+    if options[:randomize_rooms_map_friendly]
+      # The map rando won't necessarily place the rooms containing the normal bosses/portraits/villagers.
+      # So forcibly enable these options so these things can be placed wherever.
+      options[:randomize_boss_souls] = true
+      options[:randomize_portraits] = true
+      options[:randomize_villagers] = true
+    end
+    
+    if room_rando?
       @checker = DoorCompletabilityChecker.new(
         game,
         options[:enable_glitch_reqs],
@@ -353,18 +365,6 @@ class Randomizer
     spoiler_log.puts "Seed: #{@seed}, Game: #{LONG_GAME_NAME}, Randomizer version: #{DSVRANDOM_VERSION}"
     spoiler_log.puts "Selected options: #{options_string}"
     spoiler_log.puts "Difficulty level: #{difficulty_settings_string}"
-    
-    if room_rando?
-      options[:unlock_boss_doors] = true
-      options[:add_magical_tickets] = true
-    end
-    if options[:randomize_rooms_map_friendly]
-      # The map rando won't necessarily place the rooms containing the normal bosses/portraits/villagers.
-      # So forcibly enable these options so these things can be placed wherever.
-      options[:randomize_boss_souls] = true
-      options[:randomize_portraits] = true
-      options[:randomize_villagers] = true
-    end
     
     apply_pre_randomization_tweaks()
     
